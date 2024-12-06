88ee0aa340ec73b8863ec56efdaf15f7a97eae17
soru <ityouna007@gmail.com>
2024-12-06 14:04:51 +0900

    fix the error using GitHub Copilot

--- resources/views/layaouts/auth.blade.php
+++ resources/views/layaouts/auth.blade.php
@@ -3,12 +3,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>@yield("title","E-Com")</title>
-  <link href="{{ asset("assets/css/bootstrap.min.css")}}" rel="stylesheet">
-  @yield("style")
+  <title>@yield('title', 'E-Com')</title>
+  <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+  @yield('style')
 </head>
-<body class ="d-flex align-items-center py-4 bg-body-tertiary">
-  @yield("content")
-  <script src="{{ asset("assets/js/bootstrap.bundle.min.js")}}"></script>
+<body class="d-flex align-items-center py-4 bg-body-tertiary">
+  @yield('content')
+  <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
