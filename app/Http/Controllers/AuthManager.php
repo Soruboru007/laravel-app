947b8ded237e63ef3f292639429f9897133feb59
soru <ityouna007@gmail.com>
2024-12-06 13:59:17 +0900

    add auth.blade.php & login.blade.php but error

---
+++ app/Http/Controllers/AuthManager.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthManager extends Controller
+{
+  function login()
+  {
+
+  }
+
+  function loginpost(Request $request)
+  {
+
+  }
+  function register()
+  {
+
+  }
+  function registerpost(Request $request)
+  {
+
+  }
+}
