96eca29806b966d16b598a00c870379673985db0
soru <ityouna007@gmail.com>
2024-12-08 19:18:43 +0900

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/{username}', function (string $username) {
+    return view('welcome', ['username' => $username]);
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+Route::get('/user/{id}', function () {
     return view('welcome');
 });
