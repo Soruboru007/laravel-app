6f259283ee9ecd0d811ad38717c13f5cb00a200a
soru <ityouna007@gmail.com>
2024-12-09 03:22:24 +0900

    remove some line

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{username}', function (string $username) {
-    return view('welcome', ['username' => $username]);
-});
-
-Route::get('/login', function () {
-    return view('auth.login');
-});
-Route::get('/user/{id}', function () {
+Route::get('/', function () {
     return view('welcome');
-});
+})->name(name: 'home');
