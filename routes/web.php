323bf09e7ed58db5bd7ab96d309077611b220649
soru <ityouna007@gmail.com>
2024-12-10 10:42:16 +0900

    error too

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name(name: 'home');
+
+Route::get(uri: 'login', action: [AuthManager::class, 'login'])->name(name: 'login');
+
+Route::post(uri: 'login', action: [AuthManager::class, 'loginpost'])->name(name: 'loginpost');
+
+Route::get(uri: 'register', action: [AuthManager::class, 'register'])->name(name: 'register');
+
+Route::post(uri: 'register', action: [AuthManager::class, 'registerpost'])->name(name: 'registerpost');
